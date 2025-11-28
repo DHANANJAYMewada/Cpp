eafd8dbbb5ff11a61ad88078292e31770a5e3103
Dhananjay Chouhan <dhananjaymewada62@gmail.com>
2025-11-29 00:51:16 +0530

    Update 01.cpp

--- 01.cpp
+++ 01.cpp
@@ -1,5 +1,5 @@
 // singly linked list 
-//  static_way creat of linked list
+//  static_way create of linked list
 
 #include<iostream>
 using namespace std;
@@ -18,4 +18,5 @@ Node a(44);
 cout<<a.data<<endl;
 cout<<a.next<<endl;
 return 0;
+
 }
